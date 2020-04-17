ca9ef7f1076383a4877abfa630806bad0c978545
hasegawa-tomoki <github@speedstars.jp>
2020-04-17 19:56:22 +0000

    Implement comparison operators

--- tcc.c
+++ tcc.c
@@ -18,6 +18,7 @@ struct Token {
   Token *next;
   int val;
   char *str;
+  int len;
 };
 
 Token *token;
@@ -44,17 +45,16 @@ void error_at(char *loc, char *fmt, ...){
   exit(1);
 }
 
-
-bool consume(char op){
-  if (token->kind != TK_RESERVED || token->str[0] != op){
+bool consume(char *op){
+  if (token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len)){
     return false;
   }
   token = token->next;
   return true;
 }
 
-bool expect(char op){
-  if (token->kind != TK_RESERVED || token->str[0] != op){
+bool expect(char *op){
+  if (token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len)){
     error_at(token->str, "expected '%c'", op);
   }
   token = token->next;
@@ -73,12 +73,17 @@ bool at_eof(){
   return token->kind == TK_EOF;
 }
 
-Token *new_token(TokenKind kind, Token *cur, char *str){
+Token *new_token(TokenKind kind, Token *cur, char *str, int len){
   Token *tok = calloc(1, sizeof(Token));
   tok->kind = kind;
   tok->str = str;
+  tok->len = len;
   cur->next = tok;
   return tok;
+}
+
+bool startswith(char *p, char *q){
+  return memcmp(p, q, strlen(q)) == 0;
 }
 
 Token *tokenize(){
@@ -92,23 +97,31 @@ Token *tokenize(){
       p++;
       continue;
     }
+    
+    if (startswith(p, "==") || startswith(p, "!=") || startswith(p, "<=") || startswith(p, ">=")){
+      cur = new_token(TK_RESERVED, cur, p, 2);
+      p += 2;
+      continue;
+    } 
 
-    if (strchr("+-*/()", *p)){
-      cur = new_token(TK_RESERVED, cur, p);
+    if (strchr("+-*/()<>", *p)){
+      cur = new_token(TK_RESERVED, cur, p, 1);
       p++;
       continue;
     }
 
     if (isdigit(*p)){
-      cur = new_token(TK_NUM, cur, p);
+      char *q = p;
+      cur = new_token(TK_NUM, cur, p, 0);
       cur->val = strtol(p, &p, 10);
+      cur->len = p - q;
       continue;
     }
 
     error_at(p, "expected a number (2)");
   }
 
-  new_token(TK_EOF, cur, p);
+  new_token(TK_EOF, cur, p, 0);
   return head.next;
 }
 
@@ -117,6 +130,10 @@ typedef enum {
   ND_SUB, // -
   ND_MUL, // *
   ND_DIV, // /
+  ND_EQ,  // == 
+  ND_NE,  // !=
+  ND_LT,  // <
+  ND_LE,  // <=
   ND_NUM, // integer
 } NodeKind;
 
@@ -147,18 +164,60 @@ Node *new_node_num(int val){
 }
 
 Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
 Node *mul();
 Node *unary();
 Node *primary();
 
-// expr = mul ("+" mul | "-" mul)*
+// expr       = equality
 Node *expr(){
+  Node *node = equality();
+}
+
+// equality   = relational ("==" relational | "!=" relational)*
+Node *equality (){
+  Node *node = relational();
+
+  for (;;){
+    if (consume("==")){
+      node = new_node(ND_EQ, node, relational());
+    } else if (consume("!=")){
+      node = new_node(ND_NE, node, relational());
+    } else {
+      return node;
+    }
+  }
+}
+
+// relational = add ("<" add | "<=" add | ">" add | ">=" add)*
+Node *relational (){
+  Node *node = add();
+
+  for (;;){
+    if (consume("<=")){
+      node = new_node(ND_LE, node, add());
+    } else if (consume("<")){
+      node = new_node(ND_LT, node, add());
+    } else if (consume(">=")){
+      node = new_node(ND_LE, add(), node);
+    } else if (consume(">")){
+      node = new_node(ND_LT, add(), node);
+    } else {
+      return node;
+    }
+  }
+}
+
+// add = mul ("+" mul | "-" mul)*
+Node *add(){
   Node *node = mul();
 
   for (;;){
-    if (consume('+')){
+    if (consume("+")){
       node = new_node(ND_ADD, node, mul());
-    } else if (consume('-')){
+    } else if (consume("-")){
       node = new_node(ND_SUB, node, mul());
     } else {
       return node;
@@ -173,9 +232,9 @@ Node *mul(){
   Node *node = unary();
   
   for (;;){
-    if (consume('*')){
+    if (consume("*")){
       node = new_node(ND_MUL, node, unary());
-    } else if (consume('/')){
+    } else if (consume("/")){
       node = new_node(ND_DIV, node, unary());
     } else {
       return node;
@@ -187,10 +246,10 @@ Node *mul(){
 // 下記実装は
 // unary = ("+" | "-")* primary
 Node *unary(){
-  if (consume('+')){
+  if (consume("+")){
     return unary();
   }
-  if (consume('-')){
+  if (consume("-")){
     return new_node(ND_SUB, new_node_num(0), unary());
   }
   return primary();
@@ -199,9 +258,9 @@ Node *unary(){
 
 // primary = "(" expr ")" | num
 Node *primary(){
-  if (consume('(')){
+  if (consume("(")){
     Node *node = expr();
-    expect(')');
+    expect(")");
 
     return node;
   }
@@ -234,6 +293,22 @@ void gen(Node *node){
     case ND_DIV:
       printf("  cqo\n");
       printf("  div rdi\n");
+      break;
+    case ND_EQ:  // == 
+      printf("  cmp rax, rdi\n");
+      printf("  sete al\n");
+      break;
+    case ND_NE:  // !=
+      printf("  cmp rax, rdi\n");
+      printf("  setne al\n");
+      break;
+    case ND_LT:  // <
+      printf("  cmp rax, rdi\n");
+      printf("  setl al\n");
+      break;
+    case ND_LE:  // <=
+      printf("  cmp rax, rdi\n");
+      printf("  setle al\n");
       break;
   }
 
--- test.sh
+++ test.sh
@@ -26,6 +26,17 @@ assert 4 '(3+5)/2'
 assert 10 '-10+20'
 assert 10 '- -10'
 assert 10 '- - +10'
-
+assert 1 '1==1'
+assert 0 '2==1'
+assert 1 '1!=2'
+assert 0 '1!=1'
+assert 1 '1<=2'
+assert 1 '1<=1'
+assert 0 '2<=1'
+assert 1 '1<2'
+assert 0 '2<1'
+assert 1 '2>1'
+assert 0 '1>2'
+assert 2 '(1 > 2) + (2 > 1) + (1 == 1)'
 
 echo OK
