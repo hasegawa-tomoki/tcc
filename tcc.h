a3672a46ed42d54247d7559eb490fe3ae4e69847
hasegawa-tomoki <github@speedstars.jp>
2020-04-19 01:44:22 +0900

    Remove old comment

--- tcc.h
+++ tcc.h
@@ -78,16 +78,3 @@ Node *primary();
 // codegen.c
 
 void gen(Node *node);
-
-/*
-program    = stmt*
-stmt       = expr ";"
-expr       = assign
-assign     = equality ("=" assign)?
-equality   = relational ("==" relational | "!=" relational)*
-relational = add ("<" add | "<=" add | ">" add | ">=" add)*
-add        = mul ("+" mul | "-" mul)*
-mul        = unary ("*" unary | "/" unary)*
-unary      = ("+" | "-")? primary
-primary    = num | ident | "(" expr ")"
-*/
